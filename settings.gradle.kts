8e917eed6efc271487a121948a6ac3f40d5a82fe
Colin Shelton <sheltontechcto@gmail.com>
2024-07-06 19:10:01 -0500

    change module name to kwikstart-utils.... again.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+// Must disable when module names contain dashes
+//enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 pluginManagement {
     repositories {
         google()
@@ -10,19 +11,19 @@ pluginManagement {
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
-        mavenLocal()
+//        mavenLocal()
         google()
         mavenCentral()
 
-//        maven {
-//            url = uri("https://maven.pkg.github.com/sheltek/KwikStart-utils")
-//            credentials {
-//                username = System.getenv("REPO_READ_USER") ?: System.getenv("GH_READ_USERNAME")
-//                password = System.getenv("REPO_READ_TOKEN") ?: System.getenv("GH_READ_PASSWORD")
-//            }
-//        }
+        maven {
+            url = uri("https://maven.pkg.github.com/sheltek/KwikStart-utils")
+            credentials {
+                username = System.getenv("REPO_READ_USER") ?: System.getenv("GH_READ_USERNAME")
+                password = System.getenv("REPO_READ_TOKEN") ?: System.getenv("GH_READ_PASSWORD")
+            }
+        }
     }
 }
 
 rootProject.name = "KwikStart-Utils"
-include(":lib")
+include(":kwikstart-utils")
